70c2ebbee2abd4a1e5499b99aab951a4371657df
Mauricio <maurogallo@gmail.com>
2019-07-26 00:01:09 -0500

    validar las imagenes subidas

--- api-rest-laravel/app/Http/Controllers/UserController.php
+++ api-rest-laravel/app/Http/Controllers/UserController.php
@@ -196,27 +196,34 @@ class UserController extends Controller
 
         $image = $request->file('file0');
 
+        // Validacion de imagen
+
+        $validate = \Validator::make($request->all(), [
+            'file0' => 'required|image|mimes:jpg,jpeg,png,gif'
+        ]);
+
         // Guardar imagen
 
-        if ($image) {
-            $image_name = time().$image->getClientOriginalName();
+        if (!$image || $validate->fails()) {
+            $data = array(
+                'code' => 400,
+                'status' => 'error',
+                'message' => 'Error al subir imagen.'
+            );
+        } else {
+            $image_name = time() . $image->getClientOriginalName();
             \Storage::disk('users')->put($image_name, \File::get($image));
 
             $data = array(
                 'code' => 200,
                 'status' => 'success',
                 'image' => $image_name
-                 );
-        } else {
-
-            $data = array(
-                'code' => 400,
-                'status' => 'error',
-                'message' => 'Error al subir imagen.'
             );
         }
 
 
         return response($data, $data['code'])->header('Content-Type', 'text/plain');
     }
+
+
 }
