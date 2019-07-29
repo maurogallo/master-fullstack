6679e823417a15b4d8936524f7e006b57dfdc487
Mauricio <maurogallo@gmail.com>
2019-07-28 23:27:36 -0500

    Creacion compoentes del proyecto

--- blog-angular/src/app/app.module.ts
+++ blog-angular/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { routing, appRoutingProviders } from './app.routing';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
--- blog-angular/src/app/app.routing.ts
+++ blog-angular/src/app/app.routing.ts
@@ -5,13 +5,16 @@ import { Routes, RouterModule, Route } from '@angular/router';
 // IMPORTAR COMPONENTES
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 // DEFINIR LAS RUTAS
 const appRoutes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'inicio', component: LoginComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'registro', component: RegisterComponent}
+  {path: 'registro', component: RegisterComponent},
+  {path: '**', component: ErrorComponent}
 
 ];
 
