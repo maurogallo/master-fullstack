d2d4e06e9e0cfdffd4c77320ec06c9cf1c344f5f
Mauricio <maurogallo@gmail.com>
2019-07-29 01:50:12 -0500

    Registrar al usuario

--- blog-angular/src/app/components/register/register.component.ts
+++ blog-angular/src/app/components/register/register.component.ts
@@ -11,31 +11,46 @@ import { UserService } from '../../services/user.service';
 export class RegisterComponent implements OnInit {
   public page_title: string;
   public user: user;
+  public status: string;
 
   constructor(
     private _userService: UserService
   ) {
     this.page_title = 'Registrate';
     this.user = new user(1, '', '', 'ROLE_USER', '', '', '', '');
-/*
-    public id: number,
-    public name: string,
-    public surname: string,
-    public role: string,
-    public email: string,
-    public password: string,
-    public description: string,
-    public image: string*/
-   }
+    /*
+        public id: number,
+        public name: string,
+        public surname: string,
+        public role: string,
+        public email: string,
+        public password: string,
+        public description: string,
+        public image: string*/
+  }
 
   ngOnInit() {
     console.log('componenente de registro lanzado');
     console.log(this._userService.test());
   }
 
-  onSubmit(form){
-    console.log(this.user);
-    form.reset();
+  onSubmit(form) {
+    this._userService.register(this.user).subscribe(
+      Response => {
+        if (Response.status == "success") {
+          this.status = Response.status;
+          form.reset();
+        } else {
+          this.status = 'error';
+        }
+
+      },
+      error => {
+        console.log(<any>error);
+        this.status = 'error';
+      }
+    )
+
   }
 
 }
--- blog-angular/src/app/services/user.service.ts
+++ blog-angular/src/app/services/user.service.ts
@@ -2,18 +2,29 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { user } from '../models/user';
+import { global } from './global';
 
 
 @Injectable()
 export class UserService {
-
+public url: string;
   constructor(
     public _http: HttpClient
   ){
-
+    this.url = global.url;
   }
 
   test(){
     return "hola mundo desde un servicio!!";
+  }
+
+  register(user): Observable<any>{
+    let json = JSON.stringify(user);
+    let params = 'json='+json;
+
+    let headers = new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded');
+
+    return this._http.post(this.url+'register',params, {headers: headers});
+
   }
 }
