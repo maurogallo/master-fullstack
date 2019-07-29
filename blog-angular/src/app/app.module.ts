ca27e7484006d3934812c35c5262da0c29942fde
Mauricio <maurogallo@gmail.com>
2019-07-28 23:40:38 -0500

    Entradas en el menu

--- blog-angular/src/app/app.module.ts
+++ blog-angular/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routing, appRoutingProviders } from './app.routing';
 
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { routing, appRoutingProviders } from './app.routing';
 import { HomeComponent } from './components/home/home.component';
 import { ErrorComponent } from './components/error/error.component';
 
--- blog-angular/src/app/app.routing.ts
+++ blog-angular/src/app/app.routing.ts
@@ -1,6 +1,6 @@
 // IMPORTS NECESARIOS
-import { moduleWithProviders, ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule, Route } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 // IMPORTAR COMPONENTES
 import { LoginComponent } from './components/login/login.component';
@@ -10,8 +10,8 @@ import { ErrorComponent } from './components/error/error.component';
 
 // DEFINIR LAS RUTAS
 const appRoutes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'inicio', component: LoginComponent},
+  {path: '', component: HomeComponent},
+  {path: 'inicio', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'registro', component: RegisterComponent},
   {path: '**', component: ErrorComponent}
