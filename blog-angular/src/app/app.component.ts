660feed0df738af52c1022231ee968735e5b4ba0
Mauricio <maurogallo@gmail.com>
2019-08-04 23:10:19 -0500

    Mostrar datos de usuario identifcado

--- blog-angular/src/app/app.component.ts
+++ blog-angular/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
 export class AppComponent {
-  title = 'blog-angular';
+  public title = 'blog-angular';
+  public identity;
+  public token;
+
+  constructor(
+    public _userService: UserService
+  ){
+    this.identity = this._userService.getIdentity();
+
+  }
 }
+
--- blog-angular/src/app/services/user.service.ts
+++ blog-angular/src/app/services/user.service.ts
@@ -7,38 +7,62 @@ import { global } from './global';
 
 @Injectable()
 export class UserService {
-public url: string;
+  public url: string;
+  public identity;
+  public token;
+
   constructor(
     public _http: HttpClient
-  ){
+  ) {
     this.url = global.url;
   }
 
-  test(){
+  test() {
     return "hola mundo desde un servicio!!";
   }
 
-  register(user): Observable<any>{
+  register(user): Observable<any> {
     let json = JSON.stringify(user);
-    let params = 'json='+json;
+    let params = 'json=' + json;
 
     let headers = new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded');
 
-    return this._http.post(this.url+'register',params, {headers: headers});
+    return this._http.post(this.url + 'register', params, { headers: headers });
 
   }
 
-  signup(user, gettoken = null): Observable<any>{
-    if(gettoken != null){
+  signup(user, gettoken = null): Observable<any> {
+    if (gettoken != null) {
       user.gettoken = 'true';
     }
 
     let json = JSON.stringify(user);
-    let params = 'json='+json;
+    let params = 'json=' + json;
     let headers = new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded');
 
-        return this._http.post(this.url+'login', params, {headers:headers});
+    return this._http.post(this.url + 'login', params, { headers: headers });
   }
 
+  getIdentity() {
+    let identity = JSON.parse(localStorage.getItem('identity'));
+
+    if (identity && identity != "undefined") {
+      this.identity = identity;
+    } else {
+      this.identity = null;
+    }
+    return this.identity;
+  }
+
+  getToken() {
+    let token = localStorage.getItem('token');
+
+    if (token && token != "udefined") {
+      this.token = token;
+    } else {
+      this.token = true;
+    }
+    return this.token;
+  }
 
 }
